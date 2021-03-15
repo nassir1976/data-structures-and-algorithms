e09b91616a32d2e61d521b6b3027c806b697b6e7
nassir <daniya_n@yahoo.com>
2021-03-15 04:09:13 -0700

    ziplist

--- challenges/README.md
+++ challenges/README.md
@@ -168,3 +168,32 @@ Input ll	Arg k	Output                    Arg k      Output
 - head -> [1] -> [3] -> [8] -> [2] -> X	   2	=======> 3
 - head -> [1] -> [3] -> [8] -> [2] -> X	   6	=======> Exception
 
+## Code Challenge: Class 08
+
+## Zip two linked lists.
+
+- Write a function called zipLists which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+## ## Approach & Efficiency
+
+- Given two sorted linked lists, merge them so that the resulting linked list is also sorted.
+
+- Use two iterators to scan both lists
+
+Consider two sorted linked lists as an example.
+
+
+### Solution Breakdown
+Maintain a head and a tail pointer on the merged linked list. Then choose the head of the merged linked list by comparing the first node of both linked lists. For all subsequent nodes in both lists, you choose the smaller current node and link it to the tail of the merged list, and moving the current pointer of that list one step forward.
+
+You keep doing this while there are some remaining elements in both the lists. If there are still some elements in only one of the lists, you link this remaining list to the tail of the merged list.
+
+Initially, the merged linked list is NULL.
+
+Compare the value of the first two nodes and make the node with the smaller value the head node of the merged linked list. In this example, it is 4 from head1.
+
+Since it’s the first and only node in the merged list, it will also be the tail.
+
+Then move head1 one step forward.
+<!-- Embedded whiteboard image -->
+![WhiteBoard](./assets/mergetwoLL.png)
---
+++ challenges/__tests__/ll-zip.test.js
@@ -0,0 +1,212 @@
+'use strct';
+ const Node = require('../ll-zip/node.js');
+
+const LinkedList = require('../ll-zip/ll-zip.js');
+
+
+// JEST - RUNNER / ASSERTION LIBRARY -> describe, it, expect
+
+// describe -> test suite -> usually a module
+describe('NODE CLASS', () => {
+
+  // it -> an individual test -> you can also use test()
+  it('can create a new node', () => {
+    let value = 'test value';
+    let node = new Node(value);
+
+    // expect -> assertions
+
+    // check the node value we just created
+    expect(node.value).toEqual(value);
+
+    // ensure our next is null, cause there is only one node
+    expect(node.next).toBeNull();
+  });
+});
+
+
+
+
+describe('test instantiation an empty linked list', () => {
+  it('should return a linked list', () => {
+    let ll = new LinkedList;
+    expect(ll).toBeTruthy();
+  });
+});
+
+describe('test a proper insert into the linked list', () => {
+  it('should return a linked list with a new node added', () => {
+    let ll = new LinkedList;
+    ll.head = new Node(100);
+    ll.head.next = new Node(200);
+    ll.head.next.next = new Node(300);
+    ll.head.next.next.next = new Node(400);
+    ll.insert(500);
+    expect(ll.head.value).toEqual(500);
+  });
+});
+
+describe('test The head property will properly point to the first node in the linked list', () => {
+  it('should return a value equivalent to that of the first node in the linked list', () => {
+    const ll = new LinkedList;
+    ll.insert(300);
+    ll.insert(200);
+    ll.insert(100);
+
+    expect(ll.head.value).toEqual(100);
+  });
+});
+// Can properly insert multiple nodes into the linked list
+describe('test the proper ability to insert multiple nodes into list', () => {
+  it('should return a linked list with multiple values', () => {
+    const ll = new LinkedList;
+    ll.insert(300);
+    ll.insert(200);
+    ll.insert(100);
+    expect(ll.head.value).toEqual(100);
+    expect(ll.head.next.value).toEqual(200);
+    expect(ll.head.next.next.value).toEqual(300);
+  });
+});
+//Will return true when finding a value within the linked list that exists
+describe('test the searching value are with in a linked list', () => {
+  it('return true if the serching value in a linked list ', () => {
+    const ll = new LinkedList;
+    ll.insert(300);
+    ll.insert(200);
+    ll.insert(100);
+    expect(ll.includes(300)).toBeTruthy();
+  });
+  //Will return false when searching for a value in the linked list that does not exist
+  it('should return false if target value is not present ', () => {
+    const ll = new LinkedList;
+    ll.insert(400);
+    ll.insert(200);
+    ll.insert(100);
+    expect(ll.includes(400)).toBeTruthy();
+    expect(ll.includes(900)).toEqual(false);
+  });
+});
+// Can properly return a collection of all the values that exist in the linked list
+describe('testing added a collection of new node the value of the existing linked list', () => {
+
+  it(' add a collection of new node the value of the existing linked list ', () => {
+    const ll = new LinkedList;
+    ll.insert(300);
+    ll.insert(200);
+    ll.insert(100);
+    ll.append(400);
+    ll.append(500);
+    expect(ll.head.next.next.next.value).toEqual(400);
+    expect(ll.head.next.next.next.next.value).toEqual(500);
+  });
+});
+
+//===========test appended=============
+describe(' code-challenge-06 testing  append at the end of the linked list', () => {
+  it('should add a new node to the end of the linked list', () => {
+    const ll = new LinkedList;
+    ll.insert(300);
+    ll.insert(200);
+    ll.insert(100);
+    ll.append(400);
+
+    expect(ll.head.next.next.next.value).toEqual(400);
+  });
+});
+// insertbefore
+describe('testing linked list insertBefore function', () => {
+  it('should add a new node before a target value that is the first node of the list', () => {
+    const ll = new LinkedList;
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    ll.insertBefore(1, 4);
+
+    expect(ll.head.value).toEqual(1);
+  });
+  it('should add a  new node before an existing node in the middle of the linked list', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    ll.insertBefore(3, 7);
+
+    expect(ll.head.next.next.value).toEqual(7);
+  });
+});
+//insert after
+describe('testing linked list insertAfter function', () => {
+  it('should add a new node after a target value that is a middle node in the list', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    ll.insertAfter(2, 5);
+
+    expect(ll.head.next.next.value).toEqual(5);
+  });
+  it('should add a new node before the last node of the linked list', () => {
+    const ll = new LinkedList;
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    ll.insertAfter(3, 4);
+
+    expect(ll.head.next.next.next.value).toEqual(4);
+  });
+});
+//=============code-chllenge-7  ==========
+describe('testing linked list kthFromEnd function', () => {
+  it('should return invalid input string if k is greater than the length of the list', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    expect(ll.kthFromEnd(5)).toEqual('Invalid Input Number');
+  });
+
+  it('should return invalid input string if k is the length of the list because index starts at 0', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+
+    expect(ll.kthFromEnd(4)).toEqual('Invalid Input Number');
+  });
+
+  it('should return invalid input string if k is a negative number', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+
+    expect(ll.kthFromEnd(-2)).toEqual('Invalid Input Number');
+  });
+
+  it('should return the only value if the linked list has one node and k = 0', () => {
+    const ll = new LinkedList;
+    ll.insert(1);
+
+    expect(ll.kthFromEnd(0)).toEqual(1);
+  });
+
+  it('should find the value 2 places from the end if k = 2', () => {
+    const ll = new LinkedList;
+    ll.insert(4);
+    ll.insert(3);
+    ll.insert(2);
+    ll.insert(1);
+    expect(ll.kthFromEnd(2)).toEqual(2);
+  });
+});
+
+
+
+
+
